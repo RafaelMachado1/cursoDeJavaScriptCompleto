199710ec323dc2449958231f4945fa9e8c79ec83
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2024-10-19 14:16:32 -0300

    operadores-aritmeticos

---
+++ Aula - 04 Operadores/1 - Aritméticos/script.js
@@ -0,0 +1,80 @@
+//Soma
+let soma = 5
+let soma1 = 2
+let res = soma + soma1 // res = 7 (pois é igual a 5+2)
+console.log(res)
+
+//Subtração
+let subtracao = 5
+let subtracao1 = 2
+let res1 = subtracao - subtracao1 //res1 = 3 (pois é igual a 5-2)
+console.log(res1)
+
+//Multiplicação
+let mult = 5
+let mult1 = 2
+let res2 = mult * mult1 // res2 = 10 (pois é igual a 5*2)
+console.log(res2)
+
+//Exponenciação
+let expon = 5
+let expon1 = 2
+let res3 = expon ** expon1 // res3 = 25 (pois é igual a 5*5)
+console.log(res3)
+
+// Divisão
+let div = 5
+let div1 = 2
+let res4 = div / div1 //res4 = 2.5 (pois é igual a 5/2)
+console.log(res4)
+
+//Módulo (ou resto da divisão)
+let restoDiv = 5
+let restoDiv1 = 2
+let res5 = restoDiv % restoDiv1 // res5 = 1 (pois é o resto de 5/2)
+console.log(res5)
+
+//--------------------------------------------------------------------------------
+//Operando e atribuindo
+//Podemos calcular e atribuir um valor de uma vez só, usando as sintaxes abaixo.
+//Esta forma só funciona com variáveis let, pois precisamos atualizar o valor.
+
+//Soma
+let a = 5
+let b = 2
+b += a // b=7 (pois b = b+a)
+console.log(b)
+
+//Subtração
+let c = 5
+let d = 2
+d -= c // d=-3 (pois d = d-c)
+console.log(d)
+
+//Multiplicação
+let e = 5
+let f = 2
+f *= e // f = 10 (pois f = f*e)
+console.log(f)
+
+//Divisão
+let g = 5
+let h = 10
+h /= g // h = 2 (pois h = h/g)
+console.log(h)
+
+//---------------------------------------------------------------------------------
+//Incremento e Decremento
+//Especificamente com soma e subtração, temos uma sintaxe para subir (+1)
+//ou descer (-1) o valor de uma variável numérica
+
+//Incremento
+let incremento = 5
+incremento++ //incremento = incremento +1
+console.log(incremento) //incremento = 6
+
+//Decremento
+let decremento = 5
+decremento-- //decremento = decremento-1
+console.log(decremento) // decremento = 4
+
